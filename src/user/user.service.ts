581df053dd9acc1da89a5348068edce1b5c4b80e
alvaro <arommel296@g.educaand.es>
2024-05-13 18:58:07 +0200

    orm tryout

---
+++ src/user/user.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  createUser(createUserDto: CreateUserDto): Promise<User> {
+    const user: User = new User();
+    user.username = createUserDto.username;
+    user.email = createUserDto.email;
+    user.password = createUserDto.password;
+    return this.userRepository.save(user);
+  }
+
+  /**
+   * this function is used to get all the user's list
+   * @returns promise of array of users
+   */
+  findAllUser(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  /**
+   * this function used to get data of use whose id is passed in parameter
+   * @param id is type of number, which represent the id of user.
+   * @returns promise of user
+   */
+  viewUser(id: number): Promise<User> {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  /**
+   * this function is used to updated specific user whose id is passed in
+   * parameter along with passed updated data
+   * @param id is type of number, which represent the id of user.
+   * @param updateUserDto this is partial type of createUserDto.
+   * @returns promise of udpate user
+   */
+  updateUser(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    const user: User = new User();
+    user.id = id;
+    user.username = updateUserDto.username;
+    user.email = updateUserDto.email;
+    user.password = updateUserDto.password;
+    return this.userRepository.save(user);
+  }
+
+  /**
+   * this function is used to remove or delete user from database.
+   * @param id is the type of number, which represent id of user
+   * @returns nuber of rows deleted or affected
+   */
+  removeUser(id: number): Promise<{ affected?: number }> {
+    return this.userRepository.delete(id);
+  }
+}
