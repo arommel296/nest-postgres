f10849b8b2a1c0e3f4d2d2db68f11f16a19db7a5
arommel296 <alvaro97romero@gmail.com>
2024-05-15 13:42:11 +0200

    unique email validation

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -11,19 +11,23 @@ import * as bcrypt from 'bcrypt';
 export class UserService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
-  ) {}
+  ) { }
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     const user: User = new User();
     user.username = createUserDto.username;
+
+    const email = await this.userRepository.find({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+    if (email.length>0) {
+      throw new ConflictException('User already exist');
+    }
     user.email = createUserDto.email;
     user.password = await bcrypt.hash(createUserDto.password, 10);
 
-    // if (bcrypt.compare(user.password, await bcrypt.hash(createUserDto.password, 10))) {
-    //   user.password = createUserDto.password;
-    //   // const { password, ...result } = user;
-    //   return result;
-    // }
     return this.userRepository.save(user);
   }
 
@@ -36,7 +40,7 @@ export class UserService {
   }
 
   findOne(email: string): Promise<User> {
-    return this.userRepository.findOneBy({email});
+    return this.userRepository.findOneBy({ email });
   }
 
   updateUser(id: number, updateUserDto: UpdateUserDto): Promise<User> {
